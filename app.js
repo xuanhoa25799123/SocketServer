cd6286716fb9d3a1cca9679703d8b32f5d5a3a2b
xuanhoa25799123 <90512018+xuanhoa25799123@users.noreply.github.com>
2024-05-06 22:42:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ io.on("connection", (socket) => {
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-server.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
